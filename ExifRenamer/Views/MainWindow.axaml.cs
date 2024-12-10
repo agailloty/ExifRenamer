96b4449fb6b10e5817922a863212683733dd56ec
agailloty <mr.clerisaxel@hotmail.fr>
2024-12-10 22:29:54 +0100

    adjusting screen size

--- ExifRenamer/Views/MainWindow.axaml.cs
+++ ExifRenamer/Views/MainWindow.axaml.cs
@@ -7,6 +7,12 @@ namespace ExifRenamer.Views
         public MainWindow()
         {
             InitializeComponent();
+            // Get screen information
+            var screen = Screens.Primary;
+
+            // Set width and height as percentages of the screen
+            Width = screen.WorkingArea.Width * 0.4; 
+            Height = screen.WorkingArea.Height * 0.6;
         }
     }
 }
