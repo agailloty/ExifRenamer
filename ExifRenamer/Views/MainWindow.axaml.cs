9ef8549510acdb148069dd0473c1aa947265d9e3
agailloty <mr.clerisaxel@hotmail.fr>
2025-01-11 22:04:39 +0100

    enable full screen

--- ExifRenamer/Views/MainWindow.axaml.cs
+++ ExifRenamer/Views/MainWindow.axaml.cs
@@ -9,11 +9,6 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        // Get screen information
-        var screen = Screens.Primary;
-        // Set width and height as percentages of the screen
-        Width = screen.WorkingArea.Width * 0.5;
-        Height = screen.WorkingArea.Height * 0.6;
         InitializeComponent();
         var dialogService = new DialogService(this);
         DataContext = new MainWindowViewModel(dialogService);
