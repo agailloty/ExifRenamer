a5663eff3d55720adfe2db6716b115a70c0b94f4
agailloty <mr.clerisaxel@hotmail.fr>
2025-01-13 20:08:08 +0100

    adding IsBusy state to manage background tasks

--- ExifRenamer/ViewModels/ViewModelBase.cs
+++ ExifRenamer/ViewModels/ViewModelBase.cs
@@ -1,7 +1,29 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ExifRenamer.ViewModels;
 
 public class ViewModelBase : ObservableObject
 {
+    private bool _isBusy;
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set
+        {
+            if (_isBusy != value)
+            {
+                _isBusy = value;
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
