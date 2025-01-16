005b9234b57fd5a7ebc8602d06c279c3240ec505
agailloty <mr.clerisaxel@hotmail.fr>
2025-01-16 21:47:17 +0100

    make rename previewer a usercontrol

---
+++ ExifRenamer/Views/RenamePreviewer.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ExifRenamer.Views;
+
+public partial class RenamePreviewer : UserControl
+{
+    public RenamePreviewer()
+    {
+        InitializeComponent();
+    }
+}
